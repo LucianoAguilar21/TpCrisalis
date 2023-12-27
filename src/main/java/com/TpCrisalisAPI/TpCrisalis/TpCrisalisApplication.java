1a33c0c11487a1e1371664ff7b046c15d00c0528
Luciano Aguilar <lucianodavidaguilar21@gmail.com>
2023-12-27 00:12:37 -0300

    first commit, init spring boot project

---
+++ src/main/java/com/TpCrisalisAPI/TpCrisalis/TpCrisalisApplication.java
@@ -0,0 +1,13 @@
+package com.TpCrisalisAPI.TpCrisalis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpCrisalisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpCrisalisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TpCrisalisAPI/TpCrisalis/TpCrisalisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TpCrisalisAPI.TpCrisalis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpCrisalisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
